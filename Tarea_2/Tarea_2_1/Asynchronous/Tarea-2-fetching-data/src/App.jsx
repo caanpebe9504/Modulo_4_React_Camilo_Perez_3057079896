ff234756764bc6cd89e00a89073500c54a015aa5
Camilo Andrés Pérez Beltrán <cperezbe5545@gmail.com>
2024-09-27 10:24:56 -0500

    revision de funcion asincrona

--- Tarea_2/Tarea_2_1/Asynchronous/Tarea-2-fetching-data/src/App.jsx
+++ Tarea_2/Tarea_2_1/Asynchronous/Tarea-2-fetching-data/src/App.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react'
+import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
@@ -16,10 +16,6 @@ function App() {
   
     setCurrentArtistInfo(artistInfo.results[0])
   }
-
-  useEffect(() => {
-    randomArtistInfo()
-  }, [])
 
   return (
 
