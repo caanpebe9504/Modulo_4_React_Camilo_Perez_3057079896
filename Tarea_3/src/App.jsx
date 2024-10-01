a741a1eda063f29018953ea11c8bcc8c890ba5a6
Camilo Andrés Pérez Beltrán <cperezbe5545@gmail.com>
2024-09-30 21:10:14 -0500

    tarea 3 versión final

--- Tarea_3/src/App.jsx
+++ Tarea_3/src/App.jsx
@@ -7,69 +7,6 @@ import React from "react"
 
 import CharacterCard from './Ts_Files/CharacterCard'
 
-import "./styles/styles.css"
-/*
-function App() {
-  
-  const characters = [ {
-    
-    title :"Nicky Minaj",
-    image : "../public/Nicky_Minaj_Pic.jpg",
-    genre : "Femenino",
-    currentStatus : "Activa",
-    profession : "rapera"
-  },
-
-  {
-    
-    title :"Joyhauser",
-    image : "../public/Joyhauser_pic.jpg",
-    genre : "Masculino",
-    currentStatus : "Activos",
-    profession : "Techno duo"
-  },
-
-  {
-    
-    title :"Avicii",
-    image : "../public/Avicii_pic.jpg",
-    genre : "Masculino",
-    currentStatus : "Fallecido",
-    profession : "DJ"
-  },
-
-  ]
-  
-  return (
-    
-    <div id = "card_container">
-
-      {characters.map((character,position) => (
-
-        <div key={position} id="card">
-
-        <CharacterCard   
-
-          position = {position}
-          title = {character.title} 
-          image = {character.image} 
-          genre = {character.genre} 
-          currentStatus = {character.currentStatus} 
-          profession = {character.profession}
-
-        />  
-        </div>
-      ))}
-
-    </div>
-    
-  );
-
-}
-
-
-export default App
-*/
 function App() {
   
   return (
