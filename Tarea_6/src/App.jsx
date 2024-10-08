de5d4bb96ca66406ddb701328fe8589fb0939a88
Camilo Andrés Pérez Beltrán <cperezbe5545@gmail.com>
2024-10-07 19:45:18 -0500

    Tarea 6 creada

---
+++ Tarea_6/src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+  return(
+    <div>
+      <h1>Hola mundo</h1>
+      <a href="/">Link google</a>
+      <Link />
+      <NavLink/>
+    </div>
+  )
+}
+
+export default App
