589d66555b9ce8b0d510c2c4c28036315b863fa4
Camilo Andrés Pérez Beltrán <cperezbe5545@gmail.com>
2024-10-09 20:21:55 -0500

    Tarea 6 completa

--- Tarea_6/src/App.jsx
+++ Tarea_6/src/App.jsx
@@ -1,18 +1,65 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState, useEffect } from 'react'
+import "./App_styles.css"
+import CharacterCard from "./components/CharacterCard"
+
 
 function App() {
 
+  const [ charactersList, setCharacterList ] = useState([])
+  const [page, setPage] = useState(1)
+  const [info, setInfo] = useState([])
+  const [animate, setAnimate] = useState(false)
+
+    useEffect(() => {
+    
+      const url = `https://rickandmortyapi.com/api/character/?page=${page}`
+
+      fetch(url) 
+      .then((apiResponse) => apiResponse.json())
+      .then((userData) => {
+
+        setCharacterList(userData.results)
+        setInfo(userData.info)
+
+      })
+      .catch((error) => console.error("Error fetching data:", error));
+    }, [page])
+
+    const nextPage = () => {
+      if(info.next){
+        setAnimate(true)
+        setPage((nextPage) => nextPage + 1)
+      }
+    }
+
+    const prevPage = () => {
+      if(info.prev){
+        setAnimate(true)
+        setPage((prevPage) => prevPage - 1)
+      }
+    }
+
+
   return(
     <div>
-      <BrowserRouter>
-        <Routes>
-          <Route><Route/>
-        <Routes/>
-      <BrowserRouter/>
-      
+        <h1>Personajes de Rick & Morty</h1>
+      <div id="container">
+        {charactersList.map((character) => (
+            <CharacterCard 
+              key={character.id} 
+              name={character.name} 
+              image={character.image} 
+              status={character.status} 
+              create={character.created}
+              animate={animate}
+            />
+        ))}
+      </div>
+
+      <div id="page_section">
+        <button onClick={prevPage}>Anterior</button>
+        <button onClick={nextPage}>Siguiente</button>  
+      </div>
     </div>
   )
 }
