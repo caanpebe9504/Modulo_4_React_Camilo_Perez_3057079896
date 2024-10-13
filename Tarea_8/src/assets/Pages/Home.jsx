48fd1c70420055f2653d371b2594d6558852acff
Camilo Andrés Pérez Beltrán <cperezbe5545@gmail.com>
2024-10-13 12:51:22 -0500

    Tarea 8 adelanto

--- Tarea_8/src/assets/Pages/Home.jsx
+++ Tarea_8/src/assets/Pages/Home.jsx
@@ -2,7 +2,11 @@ import React from 'react'
 
 const Home = () => {
   return (
-    <div><h1>Welcome to the Rick & Morty fandom page!</h1></div>
+    <div className="flex items-center justify-center h-screen bg-gradient-to-r from-green-400 to-blue-500">
+      <h1 className="text-4xl font-bold text-white shadow-lg">Welcome to the Rick & Morty fandom page!</h1>
+      
+
+    </div>
   )
 }
 
